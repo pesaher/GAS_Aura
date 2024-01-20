9518341957a48dbc4d9b91b69f359dbb9b9ad2b0
pesaher <pesaher@gmail.com>
2024-01-20 21:48:10 +0100

    Instand and duration application policy

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -32,8 +32,36 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, const TSubclassO
 
 void AAuraEffectActor::OnBeginOverlap(AActor* TargetActor)
 {
+	if (InstantGameplayEffect.EffectApplicationPolicy == EAuraEffectPolicy::OnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffect.GameplayEffectClass);
+	}
+
+	if (DurationGameplayEffect.EffectApplicationPolicy == EAuraEffectPolicy::OnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffect.GameplayEffectClass);
+	}
+
+	if (InfiniteGameplayEffect.EffectApplicationPolicy == EAuraEffectPolicy::OnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffect.GameplayEffectClass);
+	}
 }
 
 void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
 {
+	if (InstantGameplayEffect.EffectApplicationPolicy == EAuraEffectPolicy::OnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffect.GameplayEffectClass);
+	}
+
+	if (DurationGameplayEffect.EffectApplicationPolicy == EAuraEffectPolicy::OnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffect.GameplayEffectClass);
+	}
+
+	if (InfiniteGameplayEffect.EffectApplicationPolicy == EAuraEffectPolicy::OnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffect.GameplayEffectClass);
+	}
 }
